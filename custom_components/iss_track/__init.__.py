a630e25f57a51481d4fd1d00a312c26c0d22e3b5
mattbratt <mattbratt@gmail.com>
2025-04-02 22:00:16 -0400

    Update __init.__.py

--- custom_components/iss_track/__init.__.py
+++ custom_components/iss_track/__init.__.py
@@ -7,6 +7,8 @@ DOMAIN = "iss_track"
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up ISS Tracker from a config entry."""
     hass.data.setdefault(DOMAIN, {})
+    # Serve static files from www directory
+    hass.http.register_static_path("/local/iss_track", hass.config.path("custom_components/iss_track/www"), cache_ok=True)
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
