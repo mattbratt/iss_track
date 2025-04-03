47d38c4c945e1d48668b6bdba607f73af76ab3d1
mattbratt <mattbratt@gmail.com>
2025-04-02 21:56:58 -0400

    Update sensor.py

--- custom_components/iss_track/sensor.py
+++ custom_components/iss_track/sensor.py
@@ -1,11 +1,13 @@
+"""Sensor platform for ISS Tracker."""
 from homeassistant.components.sensor import SensorEntity
-from homeassistant.const import LENGTH_KILOMETERS, SPEED_KILOMETERS_PER_HOUR
+from homeassistant.helpers.entity import Entity
+import satellitejs
 
-async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
-    async_add_entities([ISSTrackSensor()])
+class ISSTrackSensor(SensorEntity):
+    """Representation of an ISS Tracker Sensor."""
 
-class ISSSensor(SensorEntity):
-    def __init__(self):
+    def __init__(self, hass):
+        self._hass = hass
         self._state = None
         self._attributes = {}
 
@@ -22,11 +24,19 @@ class ISSSensor(SensorEntity):
         return self._attributes
 
     async def async_update(self):
-        # Fetch ISS position (you’d need to adapt the JavaScript logic to Python)
-        self._state = "Above Earth"
+        """Fetch new state data for the sensor."""
+        # This would ideally fetch TLE data and compute position
+        # For simplicity, we'll simulate it here
+        tle_line1 = "1 49044U 21066A   25056.51033439  .00026620  00000+0  48205-3 0  9999"
+        tle_line2 = "2 49044  51.6359 140.1589 0006082 313.5519  46.4964 15.49435478203179"
+        satrec = satellitejs.twoline2satrec(tle_line1, tle_line2)
+        position_and_velocity = satellitejs.propagate(satrec, new Date())
+        gmst = satellitejs.gstime(new Date())
+        position_gd = satellitejs.eci_to_geodetic(position_and_velocity.position, gmst)
+        
+        self._state = f"{position_gd.latitude:.2f}, {position_gd.longitude:.2f}"
         self._attributes = {
-            "latitude": 0.0,
-            "longitude": 0.0,
-            "altitude": 400,
-            "speed": 27600
+            "latitude": position_gd.latitude,
+            "longitude": position_gd.longitude,
+            "altitude": position_gd.height,
         }
