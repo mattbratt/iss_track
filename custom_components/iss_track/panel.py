dd41303bfc2da460020ca4ab43ebbfdf2469021a
mattbratt <mattbratt@gmail.com>
2025-04-20 21:04:22 -0400

    Update panel.py to use async

--- custom_components/iss_track/panel.py
+++ custom_components/iss_track/panel.py
@@ -1,17 +1,17 @@
-from homeassistant.components import frontend
 from homeassistant.components.panel_custom import async_register_panel
 
 def setup_panel(hass):
-    hass.async_create_task(
-        async_register_panel(
-            hass,
-            frontend_integration=DOMAIN,
-            frontend_url_path="iss",
-            module_url="/local/iss_tracker/my_iss.html",
-            sidebar_title="ISS Tracker",
-            sidebar_icon="mdi:satellite-variant",
-            require_admin=False,
-            config={},
-            trust_external=True
-        )
+    hass.async_create_task(_async_register_panel(hass))
+
+async def _async_register_panel(hass):
+    await async_register_panel(
+        hass,
+        frontend_integration="iss_track",
+        frontend_url_path="iss",
+        module_url="/local/iss_track/my_iss.html",
+        sidebar_title="ISS Tracker",
+        sidebar_icon="mdi:satellite-variant",
+        require_admin=False,
+        config={},
+        trust_external=True
     )
